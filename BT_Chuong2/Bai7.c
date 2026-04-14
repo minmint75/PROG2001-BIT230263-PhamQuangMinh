1d9005b3220ccaf3b08cfaead96530d4e3d14092
minmint75 <demonheart75205@gmail.com>
2026-04-14 20:03:10 +0700

    bt chuong 2

---
+++ BT_Chuong2/Bai7.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    float a, b, c, max;
+
+    printf("Nhap so thu nhat a: ");
+    scanf("%f", &a);
+    printf("Nhap so thu hai b: ");
+    scanf("%f", &b);
+    printf("Nhap so thu ba c: ");
+    scanf("%f", &c);
+
+
+    if (b > max) {
+        max = b;
+    }
+
+    if (c > max) {
+        max = c;
+    }
+
+    printf("\nGia tri lon nhat trong 3 so %.2f, %.2f, %.2f la: %.2f\n", a, b, c, max);
+
+    return 0;
+}
